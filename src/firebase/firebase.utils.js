9fbd41b2a6351a3a3cd47d1de9b723f82ac1d2f1
GurkiranSingh <gurkiransinghk@gmail.com>
2022-01-22 20:48:33 -0500

    Added env variables
    Signed-off-by: GurkiranSingh <gurkiransinghk@gmail.com>

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,27 @@
 import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
+
+const {
+    REACT_APP_apiKey,
+    REACT_APP_authDomain,
+    REACT_APP_databaseURL,
+    REACT_APP_projectId,
+    REACT_APP_storageBucket,
+    REACT_APP_messagingSenderId,
+    REACT_APP_appId,
+    REACT_APP_measurementId
+} = process.env;
     
 const config = {
-    apiKey: "AIzaSyBvHUYc3PolApTZ_OvPs-v6FYJ_EFwZZKw",
-    authDomain: "comfyhouse-bf9cc.firebaseapp.com",
-    databaseURL: "https://comfyhouse-bf9cc.firebaseio.com",
-    projectId: "comfyhouse-bf9cc",
-    storageBucket: "comfyhouse-bf9cc.appspot.com",
-    messagingSenderId: "703182675122",
-    appId: "1:703182675122:web:aa175e11515860e7927dab",
-    measurementId: "G-5MCXRV1R0Y"
+    apiKey: REACT_APP_apiKey,
+    authDomain: REACT_APP_authDomain,
+    databaseURL: REACT_APP_databaseURL,
+    projectId: REACT_APP_projectId,
+    storageBucket: REACT_APP_storageBucket,
+    messagingSenderId: REACT_APP_messagingSenderId,
+    appId: REACT_APP_appId,
+    measurementId: REACT_APP_measurementId
 };
 
 firebase.initializeApp(config);
