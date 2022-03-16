ba5431ebb7d967cb9e6993deb42f0e8e69180912
GurkiranSingh <gurkiransinghk@gmail.com>
2022-03-15 22:24:32 -0400

    Added idrsa key
    Signed-off-by: GurkiranSingh <gurkiransinghk@gmail.com>

--- .circleci/ssh_deploy.ps1
+++ .circleci/ssh_deploy.ps1
@@ -4,7 +4,7 @@ $creds = New-Object System.Management.Automation.PSCredential ($env:AWS_SERVER_U
 
 #Establishing an SFTP session
 Write-Host ${env:$AWS_PRIVATE_KEY_NAME}
-$Session = New-SFTPSession -ComputerName $env:AWS_SERVER_HOSTNAME -Credential $creds -KeyFile "~/.ssh/$(${env:$AWS_PRIVATE_KEY_NAME})"
+$Session = New-SFTPSession -ComputerName $env:AWS_SERVER_HOSTNAME -Credential $creds -KeyFile "~/.ssh/id_rsa"
 
 $path = Split-Path -parent $PSScriptRoot
 
